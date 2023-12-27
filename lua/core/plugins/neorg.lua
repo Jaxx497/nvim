cec71418727c975a49e279e9650d5be307ff718e
Jaxx497 <equus497@gmail.com>
2023-12-27 10:40:15 -0700

    Began neorg indentation config

--- lua/core/plugins/neorg.lua
+++ lua/core/plugins/neorg.lua
@@ -1,31 +1,39 @@
 return {
-	"nvim-neorg/neorg",
-	ft = "norg",
-	build = ":Neorg sync-parsers",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		require("neorg").setup({
-			load = {
-				["core.defaults"] = {}, -- Loads default behaviour
-				["core.concealer"] = {}, -- Adds pretty icons to your documents
-				["core.export"] = {},
-				["core.dirman"] = { -- Manages Neorg workspaces
-					config = {
-						workspaces = {
-							notes = "~/notes",
-						},
-					},
-				},
-			},
-			keys = {
-				vim.keymap.set(
-					"n",
-					"<leader>ne",
-					":Neorg export to-file<CR>.md<Left><Left><Left>",
-					{ desc = "[N]eorg [E]xport to file" }
-				),
-			},
-			init_open_folds = "always",
-		})
-	end,
+    "nvim-neorg/neorg",
+    ft = "norg",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        require("neorg").setup({
+            load = {
+                ["core.defaults"] = {}, -- Loads default behaviour
+                ["core.concealer"] = {
+                    config = {
+                        icon_preset = "diamond" },
+                }, -- Adds pretty icons to your documents
+                ["core.esupports.indent"] = {
+                    config = {
+                        format_on_enter = "false",
+                    }
+                },
+                ["core.export"] = {},
+                ["core.dirman"] = { -- Manages Neorg workspaces
+                    config = {
+                        workspaces = {
+                            notes = "~/notes",
+                        },
+                    },
+                },
+            },
+            keys = {
+                vim.keymap.set(
+                    "n",
+                    "<leader>ne",
+                    ":Neorg export to-file<CR>.md<Left><Left><Left>",
+                    { desc = "[N]eorg [E]xport to file" }
+                ),
+            },
+            init_open_folds = "always",
+        })
+    end,
 }
