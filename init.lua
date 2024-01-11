836560ffd414e9588752faaa8c13b67279666fb4
Jaxx497 <equus497@gmail.com>
2024-01-10 17:22:49 -0700

    Restructure

--- init.lua
+++ init.lua
@@ -1,4 +1,45 @@
-require("lazy")
-require("core")
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-print("Let the Lord Bless this sesh")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+        { import = "plugins" },
+        { import = "plugins.lsp" },
+    },
+    {
+        install = {
+            colorscheme = { "base16-colorscheme" },
+        },
+        checker = {
+            enabled = true,
+            notify = false,
+        },
+        change_detection = {
+            notify = false,
+        },
+        ui = {
+            border = "rounded",
+            size = {
+                width = 0.4,
+                height = 0.6,
+            },
+        },
+    })
+
+require("opts")
+require("keymap")
+
+print("May the Lord Bless this sesh")
