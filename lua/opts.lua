daffa92fce48e83986a3909767d20d491f2d5900
Jaxx497 <equus497@gmail.com>
2024-01-15 01:08:41 -0700

    format opts

--- lua/opts.lua
+++ lua/opts.lua
@@ -1,6 +1,5 @@
 if vim.fn.exists("$VIRTUAL_ENV") == 1 then
-	vim.g.python3_host_prog = vim.fn.substitute(vim.fn.system("which -a python3 | head -n2 | tail -n1"), "\n", "",
-		"g")
+	vim.g.python3_host_prog = vim.fn.substitute(vim.fn.system("which -a python3 | head -n2 | tail -n1"), "\n", "", "g")
 else
 	vim.g.python3_host_prog = vim.fn.substitute(vim.fn.system("which python3"), "\n", "", "g")
 end
@@ -18,8 +17,8 @@ set.conceallevel = 3
 
 vim.opt.guicursor = {
 	"v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
-	'i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
-	'r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100'
+	"i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+	"r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100",
 }
 
 -- Indentation
