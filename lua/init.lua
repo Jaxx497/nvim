d58ef3013e3696abe2d4311dc329182c04c8946e
Jaxx497 <equus497@gmail.com>
2024-01-23 01:18:11 -0700

    Upstream test

--- lua/init.lua
+++ lua/init.lua
@@ -1,13 +1,14 @@
-vim.diagnostic.config({
-	underline = true,
-	signs = true,
-	virtual_text = false,
-	float = {
-		show_header = true,
-		source = "if_many",
-		border = "rounded",
-		focusable = false,
-	},
-	update_in_insert = false, -- default to false
-	severity_sort = false, -- default to false
-})
+-- vim.diagnostic.config({
+-- 	underline = true,
+-- 	signs = true,
+-- 	virtual_text = false,
+-- 	float = {
+-- 		show_header = true,
+-- 		source = "if_many",
+-- 		border = "rounded",
+-- 		focusable = false,
+-- 	},
+-- 	update_in_insert = false, -- default to false
+-- 	severity_sort = false, -- default to false
+-- })
+
