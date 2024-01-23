e2dc6004fcb93a6b901d8ff8d4979ab747ea618d
Jaxx497 <equus497@gmail.com>
2024-01-23 01:22:00 -0700

    Removed dead code

--- init.lua
+++ init.lua
@@ -42,18 +42,18 @@ require("lazy").setup({
         },
     })
 
-vim.diagnostic.config({
-    underline = true,
-    signs = true,
-    virtual_text = false,
-    float = {
-        show_header = true,
-        source = "if_many",
-        border = "rounded",
-        focusable = false,
-    },
-    update_in_insert = false, -- default to false
-    severity_sort = false, -- default to false
-})
+-- vim.diagnostic.config({
+--     underline = true,
+--     signs = true,
+--     virtual_text = false,
+--     float = {
+--         show_header = true,
+--         source = "if_many",
+--         border = "rounded",
+--         focusable = false,
+--     },
+--     update_in_insert = false, -- default to false
+--     severity_sort = false, -- default to false
+-- })
 
 print("May the Lord Bless this sesh!!")
