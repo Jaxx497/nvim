5d4eedc1d47f109563e0557e254820a50c18053d
Jaxx497 <equus497@gmail.com>
2024-02-12 19:01:32 -0700

    bug fix

--- lua/plugins/navigator.lua
+++ lua/plugins/navigator.lua
@@ -1,6 +1,9 @@
 return {
     'mrjones2014/smart-splits.nvim',
     config = function()
+        require("smart-splits").setup({
+            multiplexer_integration = "wezterm",
+        })
         vim.keymap.set('n', '<A-h>', require('smart-splits').resize_left)
         vim.keymap.set('n', '<A-j>', require('smart-splits').resize_down)
         vim.keymap.set('n', '<A-k>', require('smart-splits').resize_up)
