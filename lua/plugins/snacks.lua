d5e5d01c468d1b5c09543d963ff2d1efaa7e0ed0
Jaxx497 <equus497@gmail.com>
2025-01-27 23:03:53 -0700

    Hopefully fixed upward scroll bug

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -22,6 +22,11 @@ return {
                 duration = { step = 10, total = 250 },
                 easing = "linear",
             },
+            animate_repeat = {
+                delay = 100, -- delay in ms before using the repeat animation
+                duration = { step = 5, total = 50 },
+                easing = "linear",
+            },
         },
         styles = {
             notification = {
